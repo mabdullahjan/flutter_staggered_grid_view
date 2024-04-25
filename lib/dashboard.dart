8f2e7f0e88ae2d5565f9cfab5fecd05201f2e011
mabdullahjan <suit.maj@gmail.com>
2024-04-25 10:38:14 +0500

    first commit

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -6,63 +6,55 @@ class Dashboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    
     return Scaffold(
-      
-        appBar: AppBar(
-          title: Text("DashBoard"),
-          centerTitle: true,
-          backgroundColor: Colors.amber,
-        ),
-        body: SingleChildScrollView(
-          child: StaggeredGrid.count(
-            crossAxisCount: 2,
-            mainAxisSpacing: 2,
-            crossAxisSpacing: 2,
-            children: [
-              StaggeredGridTile.count(
-              crossAxisCellCount: 1, 
-              mainAxisCellCount: 2, 
-              child:  Imagecontainer('assets/1.jpg'),
-              ),
-              StaggeredGridTile.count(
-              crossAxisCellCount: 1, 
-              mainAxisCellCount: 2, 
-              child:  Imagecontainer('assets/2.jpg'),
-              ),
-          
-              StaggeredGridTile.count(
-              crossAxisCellCount: 1, 
-              mainAxisCellCount: 1, 
-              child:  Imagecontainer('assets/3.jpg'),
-              ),
-              StaggeredGridTile.count(
-              crossAxisCellCount: 1, 
-              mainAxisCellCount: 1, 
-              child:  Imagecontainer('assets/2.jpg'),
-              ),
-              StaggeredGridTile.count(
-              crossAxisCellCount: 1, 
-              mainAxisCellCount: 2, 
-              child:  Imagecontainer('assets/1.jpg'),
-              ),
-              StaggeredGridTile.count(
-              crossAxisCellCount: 1, 
-              mainAxisCellCount: 2, 
-              child:  Imagecontainer('assets/2.jpg'),
-              ),
-              
-          
-            ],
+      appBar: AppBar(
+        title: Text("Dashboard"),
+        centerTitle: true,
+        backgroundColor: Colors.amber,
+      ),
+      body: SingleChildScrollView(
+        child: StaggeredGrid.count(
+          crossAxisCount: 2,
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+          children: [
+            StaggeredGridTile.count(
+              crossAxisCellCount: 1,
+              mainAxisCellCount: 1,
+              child: Imagecontainer('assets/1.jpg'),
             ),
+            StaggeredGridTile.count(
+              crossAxisCellCount: 1,
+              mainAxisCellCount: 2,
+              child: Imagecontainer('assets/2.jpg'),
+            ),
+            StaggeredGridTile.count(
+              crossAxisCellCount: 1,
+              mainAxisCellCount: 1,
+              child: Imagecontainer('assets/3.jpg'),
+            ),
+            StaggeredGridTile.count(
+              crossAxisCellCount: 1,
+              mainAxisCellCount: 1,
+              child: Imagecontainer('assets/2.jpg'),
+            ),
+            StaggeredGridTile.count(
+              crossAxisCellCount: 1,
+              mainAxisCellCount: 2,
+              child: Imagecontainer('assets/1.jpg'),
+            ),
+            StaggeredGridTile.count(
+              crossAxisCellCount: 1,
+              mainAxisCellCount: 2,
+              child: Imagecontainer('assets/2.jpg'),
+            ),
+          ],
         ),
-          
+      ),
     );
-  
-
   }
-  Imagecontainer(imgPath){
+
+  Imagecontainer(imgPath) {
     return Image.asset(
       imgPath,
       fit: BoxFit.cover,
